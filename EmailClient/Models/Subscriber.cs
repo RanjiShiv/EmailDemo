59840bbd633991e1869b5f20f1731df7987a135f
Ranjitha Shivarudraiah <rshivarudraiah@costargroup.com>
2021-07-30 14:38:07 -0400

    Add all files

---
+++ EmailClient/Models/Subscriber.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailClient.Models
+{
+    class Subscriber:PageModel
+    {
+        [BindProperty]
+        public string firstname { get; set; }
+        [BindProperty]
+        public string lastname { get; set; }
+        [BindProperty]
+        public string productname { get; set; }
+    }
+}
---
+++ EmailClient/Program.cs
@@ -0,0 +1,85 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sender = new SmtpSender(() => new SmtpClient("localhost")
+            {
+
+                EnableSsl = false,
+                /*send email using localhost
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 25
+                */
+                //Store emails in a local folder
+                DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                //Create the folder if it does not exist
+                PickupDirectoryLocation = @"C:\Emails"
+            });
+
+            //SendEmailUsingTemplateString(sender);
+            SendEmailUsingTemplateFile(sender);
+
+
+
+        }
+
+        private static async void SendEmailUsingTemplateString( SmtpSender sender)
+        {
+            StringBuilder template = new StringBuilder();
+            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("<p>Thanks for purchasing @Model.ProductName. We hope you enjoy it.</p>");
+            template.AppendLine("- The TimCo Team");
+
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+
+            var email = await Email
+                .From("tim@timco.com")
+                .To("typicalmilkyway@gmail.com", "Ranjitha Shivarudraiah")
+                .Subject("From email demo App!")
+                .UsingTemplate(template.ToString(), new { FirstName = "Ranjitha", ProductName = "Bacon-Wrapped Bacon" })
+                // .Body("This email is sent from the email demo app.")
+                .SendAsync();
+        }
+
+
+        private static async void SendEmailUsingTemplateFile(SmtpSender sender)
+        {
+            StringBuilder template = new StringBuilder();
+            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("<p>Thanks for purchasing @Model.ProductName. We hope you enjoy it.</p>");
+            template.AppendLine("- The TimCo Team");
+            //set some email properties
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+
+            //figure out a way to set the model in razor pages (currently using inline models)
+
+
+            Console.Write(Directory.GetCurrentDirectory());
+
+
+            var email = await Email
+                .From("tim@timco.com")
+                .To("typicalmilkyway@gmail.com", "Ranjitha Shivarudraiah")
+                .Subject("From email demo App!")
+                .UsingTemplateFromFile($"{Path.Combine("../../../", "Templates\\sample.cshtml")}",
+                new { FirstName = "Ranjitha", ProductName = "Bacon-Wrapped Bacon" })
+                // .Body("This email is sent from the email demo app.")
+                .SendAsync();
+        }
+
+
+    }
+}
